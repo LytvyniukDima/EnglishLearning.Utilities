dac872c18b78710ee4b51c071832fcda54d55e38
dimaLytvyniuk <dima20-12@outlook.com>
2018-11-11 20:29:49 +0200

    Add packaging new packages

--- EnvironmentTools/makePackages.sh
+++ EnvironmentTools/makePackages.sh
@@ -1,7 +1,12 @@
 version="1.0.1"
 outputFolder="../nupkg"
 
+dotnet pack ./EnglishLearning.Utilities.All/EnglishLearning.Utilities.All.csproj -p:PackageVersion=$version --output $outputFolder
+
 dotnet pack ./EnglishLearning.Utilities.Enums/EnglishLearning.Utilities.Enums.csproj -p:PackageVersion=$version --output $outputFolder
 dotnet pack ./EnglishLearning.Utilities.Expressions/EnglishLearning.Utilities.Expressions.csproj -p:PackageVersion=$version --output $outputFolder
 dotnet pack ./EnglishLearning.Utilities.Linq/EnglishLearning.Utilities.Linq.csproj -p:PackageVersion=$version --output $outputFolder
-dotnet pack ./EnglishLearning.Utilities.All/EnglishLearning.Utilities.All.csproj -p:PackageVersion=$version --output $outputFolder
+dotnet pack ./EnglishLearning.Utilities.Configurations/EnglishLearning.Utilities.Configurations.csproj -p:PackageVersion=$version --output $outputFolder
+
+dotnet pack ./EnglishLearning.Utilities.Persistence/EnglishLearning.Utilities.Persistence.csproj -p:PackageVersion=$version --output $outputFolder
+dotnet pack ./EnglishLearning.Utilities.Persistence.Mongo/EnglishLearning.Utilities.Persistence.Mongo.csproj -p:PackageVersion=$version --output $outputFolder
