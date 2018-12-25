c46dc435455089980e16ebd9064472971a889434
dimaLytvyniuk <dima20-12@outlook.com>
2018-12-25 20:19:36 +0200

    Fix AddMongoDependencies method

--- EnglishLearning.Utilities.Persistence.Mongo/Configuration/ServicesConfiguration.cs
+++ EnglishLearning.Utilities.Persistence.Mongo/Configuration/ServicesConfiguration.cs
@@ -1,13 +1,11 @@
 using EnglishLearning.Utilities.Persistence.Mongo.Contexts;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EnglishLearning.Utilities.Persistence.Mongo.Configuration
 {
     public static class ServicesConfiguration
     {
-        public static IServiceCollection AddMongoDependencies(this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddMongoDependencies(this IServiceCollection services)
         {
             services.AddScoped<MongoContext>();
             
