9a9f5fc962d55c13422f489b4007837e21103170
dimaLytvyniuk <dima20-12@outlook.com>
2019-04-06 19:06:28 +0300

    Add identity abstractions project

---
+++ EnglishLearning.Utilities.Identity.Abstractions/AuthorizeRole.cs
@@ -0,0 +1,9 @@
+namespace EnglishLearning.Utilities.Identity.Abstractions
+{
+    public enum AuthorizeRole
+    {
+        Admin,
+        BaseCustomer,
+        SubscribedCustomer
+    }
+}
---
+++ EnglishLearning.Utilities.Identity.Abstractions/EnglishLearningAuthorize.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace EnglishLearning.Utilities.Identity.Abstractions
+{
+    public class EnglishLearningAuthorizeAttribute: Attribute
+    {
+        public EnglishLearningAuthorizeAttribute()
+        {
+        }
+
+        public EnglishLearningAuthorizeAttribute(params AuthorizeRole[] roles)
+        {
+            Roles = roles;
+        }
+
+        public AuthorizeRole[] Roles { get; set; } = new[]
+        {
+            AuthorizeRole.BaseCustomer,
+            AuthorizeRole.Admin,
+            AuthorizeRole.SubscribedCustomer
+        };
+    }
+}
