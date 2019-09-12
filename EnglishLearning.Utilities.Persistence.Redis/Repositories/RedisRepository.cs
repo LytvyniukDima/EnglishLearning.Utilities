dd109e5cc1a859f4658ba464f7fbe3dfd0b517a0
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-09-12 09:43:00 +0300

    Extend key value repositories

--- EnglishLearning.Utilities.Persistence.Redis/Repositories/RedisRepository.cs
+++ EnglishLearning.Utilities.Persistence.Redis/Repositories/RedisRepository.cs
@@ -1,4 +1,5 @@
 ﻿using EnglishLearning.Utilities.Persistence.Interfaces;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace EnglishLearning.Utilities.Persistence.Redis.Repositories
@@ -22,8 +23,34 @@ namespace EnglishLearning.Utilities.Persistence.Redis.Repositories
         public string GetStringValueByKey(string key)
         {
             var value = _database.StringGet(key);
+            
+            return value;
+        }
+        
+        public void SetByteValue(string key, byte[] value)
+        {
+            _database.StringSet(key, value);
+        }
+
+        public byte[] GetByteValue(string key)
+        {
+            byte[] value = _database.StringGet(key);
 
             return value;
+        }
+        
+        public void SetObjectValue<T>(string key, T value)
+        {
+            var serializedObject = JsonConvert.SerializeObject(value);
+            _database.StringSet(key, serializedObject);
+        }
+
+        public T GetObjectValue<T>(string key)
+        {
+            string value = _database.StringGet(key);
+            var serializedObject = JsonConvert.DeserializeObject<T>(value);
+
+            return serializedObject;
         }
     }
 }
--- EnglishLearning.Utilities.Persistence/Interfaces/IKeyValueRepository.cs
+++ EnglishLearning.Utilities.Persistence/Interfaces/IKeyValueRepository.cs
@@ -4,5 +4,11 @@
     {
         void SetStringKeyValue(string key, string value);
         string GetStringValueByKey(string key);
+        
+        void SetByteValue(string key, byte[] value);
+        byte[] GetByteValue(string key);
+        
+        void SetObjectValue<T>(string key, T value);
+        T GetObjectValue<T>(string key);
     }
 }
