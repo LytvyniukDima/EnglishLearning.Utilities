a0c41dd94e6b20504cef54bda870992d6cf6569a
LytvyniukDima <dima.lytvyniuk@gmail.com>
2020-08-08 16:10:57 +0300

    Add ConvertToStringKeyDictionary extension

---
+++ src/EnglishLearning.Utilities.Linq/src/EnglishLearning.Utilities.Linq/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EnglishLearning.Utilities.Linq.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<string, TValue> ConvertToStringKeyDictionary<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary)
+        {
+            return dictionary
+                .Select(x => (key: x.Key.ToString(), value: x.Value))
+                .ToDictionary(x => x.key, x => x.value);
+        }
+    }
+}
